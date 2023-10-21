dc690dfee2208523eef3de9b0143f6804051abbe
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-21 01:22:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+for( i=0;i<10;i++) {
 
+const min = 1;
+const max = 10;
+const randomIntInRange = Math.floor(Math.random() * (max - min + 1)) + min; 
+console.log("random number="+ randomIntInRange);
+
+const baseUrl = 'https://jsonplaceholder.typicode.com/users/'
+
+fullUrl = baseUrl+randomIntInRange;
+
+
+const outPut = document.getElementById("outPut");
+
+
+fetch(fullUrl)
+      .then(response => response.json())
+      .then(json => { 
+
+
+
+  const jsonString = JSON.stringify(json);
+
+//outPut.innerHTML += jsonString;
+
+const jsonObject = JSON.parse(jsonString);
+
+// Access the 'url' property
+const friend = jsonObject.name;
+
+const id = jsonObject.id;
+
+const freinds = document.getElementById("friends");
+
+freinds.innerHTML +="<br>Name:<br>"+ friend +"<br>Id:<br>"+id;
+
+});
+
+}
+
+/* 
+
+/posts (100)
+/users (10)
+/comments (500)
+/albums (100)
+/photos (5000)
+/todos (200) 
+
+*/ 
