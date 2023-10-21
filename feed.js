6a9c37b353a0e5aebfa221f41e41d68d7165291d
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-20 21:24:13 -0600

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,1 +1,63 @@
+const min = 1;
+const max = 100;
+const randomIntInRange = Math.floor(Math.random() * (max - min + 1)) + min; 
+console.log("random number="+ randomIntInRange);
 
+const baseUrl = 'https://jsonplaceholder.typicode.com/posts/'
+
+const fullUrl = baseUrl+randomIntInRange;
+
+
+const feedContainer = document.getElementById("feedContainer");
+
+
+fetch(fullUrl)
+      .then(response => response.json())
+      .then(json => { 
+
+
+   const jsonString = JSON.stringify(json);
+
+
+outPut.innerHTML += jsonString;
+
+const jsonObject = JSON.parse(jsonString);
+
+// Access the 'url' property
+const url = jsonObject.url;
+
+const thumbNail= jsonObject.thumbnailUrl;
+
+console.log(url);
+
+const thumbnail = document.querySelector(".thumbnail");
+
+const image = document.querySelector("img");
+
+const newImage = image.setAttribute("src",url);
+
+const thumbImage =
+thumbnail.setAttribute("src",thumbNail);
+})
+
+.catch(error => {
+    console.error('Error:', error);
+});
+
+
+
+
+
+
+
+
+/* 
+
+/posts (100)
+/users (10)
+/comments (500)
+/albums (100)
+/photos (5000)
+/todos (200) 
+
+*/ 
