d7bf329721d424660e356ae127d4afb4a9eafca2
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-21 00:15:22 -0600

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,4 +1,7 @@
-/* const min = 1;
+for(let i=0;i<50;i++){
+
+
+const min = 1;
 const max = 5000;
 const randomIntInRange = Math.floor(Math.random() * (max - min + 1)) + min; 
 console.log("random number="+ randomIntInRange);
@@ -16,51 +19,33 @@ fetch(fullUrl)
       .then(json => { 
 
 
+
   const jsonString = JSON.stringify(json);
 
 
-outPut.innerHTML += jsonString;
+//outPut.innerHTML += jsonString;
 
 const jsonObject = JSON.parse(jsonString);
 
 // Access the 'url' property
 const url = jsonObject.url;
 
-const thumbNail= jsonObject.thumbnailUrl;
+const title = jsonObject.title;
 
-console.log(url);
+const imgElement = document.createElement("img");
 
-const thumbnail = document.querySelectorAll(".thumbnail");
+const container =
+document.createElement("div");
 
-const image = document.querySelectorAll(".normal");
+imgElement.src= url ;
+container.innerHTML = title ;
 
-const newImage = image.setAttribute("src",url);
+const photoContainer = getElementById("photoContainer");
 
-const thumbImage =
-thumbnail.setAttribute("src",thumbNail);
-})
+photoContainer.appendChild(imgElement);
+photoContainer.appendChild(container);
 
-.catch(error => {
-    console.error('Error:', error);
 });
-*/
-for(let i=0;i< 100;i++) {
-const photoContainer = document.getElementById("photoContainer");
-const url = "https://picsum.photos/200";
 
-//const image = document.querySelectorAll(".normal");
-
-//const newImage = image.setAttribute("src",url);
-
-// Create an img element
-const img = document.createElement("img");
-
-// Set the src and alt attributes
-img.src = url; // Replace with the actual image URL
-//img.alt = "Image Description"; // Replace with the alt text
-
-// Append the img element to the document or another container
-photoContainer.appendChild(img); // This appends it to the body of the document
 }
-        
 
